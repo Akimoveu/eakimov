dd270f6711194e3408a8130f7631163c9381406e
Akimoveu <Akimoveu@gmail.com>
2016-11-28 21:32:31 +0600

    add new Calculate.java

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,10 +1,15 @@
-package ru.job4j;
+﻿package ru.job4j;
+
+/**
+ *Class Calculate решение задачи части 001 урок 1
+ *@author akimov
+ *@since 28.11.2016
+*/
 public class Calculate {
 
 public static void main(String[] args){
 
-System.out.println("Hello World!");
+	System.out.println("Hello World!");
 
-
-}
+	}
 }
